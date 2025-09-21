cc05765b6a722c0dea8fa312e824d4b8f42e1b3d
renelps <renanlps616@gmail.com>
2025-09-21 09:45:59 -0300

    feat: add session check to dashboard page

--- src/app/(panel)/dashboard/page.tsx
+++ src/app/(panel)/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
 
+export default async function Dashboard(){
+  const session = await getSession();
 
-
-export default function Dashboard(){
+  if(!session) redirect("/");
+  
   return (
     <div>
       <h2>Dashboard</h2>
