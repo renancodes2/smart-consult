81d15d6232ed0dd062381a013619b8250608b127
renelps <renanlps616@gmail.com>
2025-09-21 09:46:28 -0300

    feat: create server function to get user data with subscription

---
+++ src/app/(panel)/dashboard/profile/_data-access/get-user.tsx
@@ -0,0 +1,27 @@
+"use server"
+
+import prisma from "@/lib/prisma"
+
+interface GetUserDataProps {
+  userId: string;
+}
+
+export async function getUserData({ userId }: GetUserDataProps){
+  try { 
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userId
+      },
+      include: {
+        subscription: true,
+      }
+    })
+
+    if(!user) return null;
+
+    return user;
+
+  }catch(err){
+    console.log(err)
+  }
+}
