05fe2087c501eab43f087b9e45884b33c28c8bc5
renelps <renanlps616@gmail.com>
2025-09-21 09:34:08 -0300

    feat: add footer component with copyright information

---
+++ src/app/(public)/_components/footer.tsx
@@ -0,0 +1,11 @@
+
+
+export function Footer(){
+
+  return (
+    <footer className="flex items-center justify-center py-3">
+      <p>Todos os direitos reservados {new Date().getFullYear()} - @RenanGabriel</p>
+    </footer>
+  )
+  
+}
